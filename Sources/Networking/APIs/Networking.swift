fa59e28188783fac79c95e900e936684fcdee7a4
Dudarev Ivan <dudarevmail@icloud.com>
2021-06-02 15:23:38 +0500

    Fetch Image

--- Sources/Networking/APIs/Networking.swift
+++ Sources/Networking/APIs/Networking.swift
@@ -7,44 +7,58 @@
 
 import Foundation
 import Combine
+import SwiftUI
 
 public struct Networking {
-    
-    public static let shared = Networking()
-    
-    private let urlPath = URLPath.shared
-    
-    public init() {}
-    
-    // Generic fetch
-    private func fetch<T: Decodable>(_ url: URL) -> AnyPublisher<T, Error> {
-              return URLSession.shared.dataTaskPublisher(for: url)
-                .map { $0.data }
-                .decode(type: T.self, decoder: JSONDecoder())
-                .receive(on: RunLoop.main)
-                .eraseToAnyPublisher()
-    }
-    
-    
-    // Fetch games
-    public func fetchGames(endPoint: EndPoint) -> AnyPublisher<[Country], Never> {
-        guard let url = urlPath.gamesURL(for: endPoint) else {
-            return Just([Country]()).eraseToAnyPublisher()
-        }
-        return fetch(url)
-            .map { (response: GamesResponse) -> [Country] in
-                response.countries }
-            .replaceError(with: [Country]())
-            .eraseToAnyPublisher()
-    }
-    
-    // Fetch archive
-    public func fetchArchive(for path: String) -> AnyPublisher<Archive, Never> {
-        guard let url = urlPath.absoluteURL(for: path) else {
-            return Just(Archive()).eraseToAnyPublisher()
-        }
-        return fetch(url)
-            .replaceError(with: Archive())
-            .eraseToAnyPublisher()
-    }
+   
+   public static let shared = Networking()
+   
+   private let urlPath = URLPath.shared
+   
+   public init() {}
+   
+   // Generic fetch
+   private func fetch<T: Decodable>(_ url: URL) -> AnyPublisher<T, Error> {
+      return URLSession.shared.dataTaskPublisher(for: url)
+         .map { $0.data }
+         .decode(type: T.self, decoder: JSONDecoder())
+         .receive(on: RunLoop.main)
+         .eraseToAnyPublisher()
+   }
+   
+   
+   // Fetch games
+   public func fetchGames(endPoint: EndPoint) -> AnyPublisher<[Country], Never> {
+      guard let url = urlPath.gamesURL(for: endPoint) else {
+         return Just([Country]()).eraseToAnyPublisher()
+      }
+      return fetch(url)
+         .map { (response: GamesResponse) -> [Country] in
+            response.countries }
+         .replaceError(with: [Country]())
+         .eraseToAnyPublisher()
+   }
+   
+   // Fetch archive
+   public func fetchArchive(for path: String) -> AnyPublisher<Archive, Never> {
+      guard let url = urlPath.absoluteURL(for: path) else {
+         return Just(Archive()).eraseToAnyPublisher()
+      }
+      return fetch(url)
+         .replaceError(with: Archive())
+         .eraseToAnyPublisher()
+   }
+   
+   // Fetch image
+   func fetchImage(for path: String) -> AnyPublisher <UIImage?, Never> {
+      guard let url = URL(string: path) else {
+         return Just(nil).eraseToAnyPublisher()
+      }
+      return
+         URLSession.shared.dataTaskPublisher(for: url)
+         .map { UIImage(data: $0.data) }
+         .replaceError(with: nil)
+         .receive(on: RunLoop.main)
+         .eraseToAnyPublisher()                         
+   }
 }
