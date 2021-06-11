15939ba338a267f52f7ee15ebe49ef2bfb137b25
Dudarev Ivan <dudarevmail@icloud.com>
2021-06-11 22:39:48 +0500

    add numberOfGames

--- Sources/Networking/Models/Games.swift
+++ Sources/Networking/Models/Games.swift
@@ -10,6 +10,7 @@ import Foundation
 public struct Games: Codable {
    
    public let countries: [Country]
+   public let numberOfGames: Int
    
    public init(countries: [Country]) {
       self.countries = countries
@@ -22,12 +23,14 @@ public struct Country: Codable, Identifiable {
    public var id: Int?
    public var name: String?
    public var shortName: String?
+   public let numberOfGames: Int?
    public var leagues: [Leagues]?
    
-   public init(id: Int?, name: String?, shortName: String?, leagues: [Leagues]?) {
+   public init(id: Int?, name: String?, shortName: String?, numberOfGames: Int?, leagues: [Leagues]?) {
       self.id = id
       self.name = name
       self.shortName = shortName
+      self.numberOfGames = numberOfGames
       self.leagues = leagues
    }
 }
@@ -38,12 +41,14 @@ public struct Leagues: Codable, Identifiable {
    public var id: Int?
    public var name: String?
    public var icon: String?
+   public var numberOfGames: Int?
    public var seasons: [Seasons]?
    
-   public init(id: Int?, name: String?, icon: String? = nil, seasons: [Seasons]?) {
+   public init(id: Int?, name: String?, icon: String? = nil, numberOfGames: Int?, seasons: [Seasons]?) {
       self.id = id
       self.name = name
       self.icon = icon
+      self.numberOfGames = numberOfGames
       self.seasons = seasons
    }
 }
@@ -53,11 +58,13 @@ public struct Seasons: Codable, Identifiable {
    
    public var id: Int?
    public var season: String?
+   public let numberOfGames: Int?
    public var archive: String?
    
-   public init(id: Int?, season: String?, archive: String?) {
+   public init(id: Int?, season: String?, numberOfGames: Int?, archive: String?) {
       self.id = id
       self.season = season
+      self.numberOfGames = numberOfGames
       self.archive = archive
    }
 }
