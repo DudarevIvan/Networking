ad531d62c959e66a5510174f8a69c430ff4e7627
Ivan Dudarev <dudarevmail@icloud.com>
2021-12-03 16:07:01 +0500

    Generic ViewModel

--- Sources/Networking/ViewModels/Hockey/HockeyViewModel.swift
+++ Sources/Networking/ViewModels/Hockey/HockeyViewModel.swift
@@ -2,40 +2,44 @@
 //  File.swift
 //  
 //
-//  Created by Ivan Dudarev on 01.12.2021.
+//  Created by Ivan Dudarev on 03.12.2021.
 //
 
 import Combine
 
-public final class FootballArchiveViewModel: ObservableObject {
+public final class HockeyViewModel: ObservableObject {
+    
+    // Shared
+    public static let shared: HockeyViewModel = .init()
     
     // Networking
     private var service = NetworkingService.shared
     
-    // Archive
-    @Published public var archive: FootballArchive = .init()
+    // Sports data
+    @Published public var hockey: Hockey = .init()
     
     // Errors
     @Published public var networkingError: NetworkingError?
     
     // Save Published
     private var cancellableSet: Set<AnyCancellable> = []
+    
     deinit {
-        for cancell in cancellableSet {
-            cancell.cancel()
+        for cancellable in cancellableSet {
+            cancellable.cancel()
         }
     }
     
-    public init(url: String) {
-        service.fetchArchive(for: url)
+    private init() {
+        self.service.fetchSport(for: Sports.Hockey)
             .eraseToAnyPublisher()
             .sink(
                 receiveCompletion: { completion in
                     if case let .failure(error) = completion {
                         self.networkingError = error
                     }},
-                receiveValue: { [unowned self] archive in
-                    self.archive = archive
+                receiveValue: { [unowned self] value in
+                    hockey = value
                 }
             )
             .store(in: &self.cancellableSet)
--- Sources/Networking/ViewModels/SportViewModel.swift
+++ Sources/Networking/ViewModels/SportViewModel.swift
@@ -2,40 +2,41 @@
 //  File.swift
 //  
 //
-//  Created by Ivan Dudarev on 01.12.2021.
+//  Created by Ivan Dudarev on 03.12.2021.
 //
 
 import Combine
 
-public final class FootballArchiveViewModel: ObservableObject {
+public final class SportViewModel<SportGen: Decodable>: ObservableObject {
     
     // Networking
     private var service = NetworkingService.shared
     
-    // Archive
-    @Published public var archive: FootballArchive = .init()
+    // Sports data
+    @Published public var data: SportGen?
     
     // Errors
     @Published public var networkingError: NetworkingError?
     
     // Save Published
     private var cancellableSet: Set<AnyCancellable> = []
+    
     deinit {
-        for cancell in cancellableSet {
-            cancell.cancel()
+        for cancellable in cancellableSet {
+            cancellable.cancel()
         }
     }
     
-    public init(url: String) {
-        service.fetchArchive(for: url)
+    public init() {
+        self.service.fetchSport(for: Sports.Football)
             .eraseToAnyPublisher()
             .sink(
                 receiveCompletion: { completion in
                     if case let .failure(error) = completion {
                         self.networkingError = error
                     }},
-                receiveValue: { [unowned self] archive in
-                    self.archive = archive
+                receiveValue: { [unowned self] value in
+                    data = value
                 }
             )
             .store(in: &self.cancellableSet)
